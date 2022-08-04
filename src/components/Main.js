60e7cc915858682796fa870afd753a8c390308be
Roman Shigapov <i3.bait@yandex.ru>
2022-08-04 23:40:11 +0700

    вывел карточки

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ function Main(props) {
   const [userDescription, setUserDescription] = React.useState('...данных');
   const [userAvatar, setUserAvatar] = React.useState(defaultAvatar);
 
-  let Counter = 1;
+  const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
     Api.getUserInfo()
@@ -16,6 +16,14 @@ function Main(props) {
         setUserName(userData.name);
         setUserDescription(userData.about);
         setUserAvatar(userData.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+    Api.getCardsList()
+      .then(cards => {
+        setCards([...cards])
       })
       .catch((err) => {
         console.log(err);
@@ -37,6 +45,21 @@ function Main(props) {
       </section>
       <section className="places" aria-label="Секция карточек мест">
         <ul className="places__grid-items">
+          {cards.map(card => {
+            return (
+            <li className="card" key={card._id}>
+              <img className="card__picture" src={card.link} alt={card.name} title="" />
+              <button className="card__delete-button button button_opacity_main"></button>
+              <div className="card__description">
+                <h2 className="card__caption">{card.name}</h2>
+                <div className="card__like-block">
+                  <button className="card__like-button button button_opacity_low" type="button" aria-label="кнопка лайк на карточке"></button>
+                  <span className="card__likes">{card.likes.length}</span>
+                </div>
+              </div>
+            </li>
+            )
+          })}
         </ul>
       </section>
     </main>
