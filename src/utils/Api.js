1910b0ee184075de9fe20902e41f91a54cb3e3e3
Roman Shigapov <i3.bait@yandex.ru>
2022-08-22 11:27:55 +0700

    P11 fix api by review

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,107 +1,101 @@
-import {api_options} from './Utils';
+import {apiOptions} from './Utils';
 
 class Api {
   constructor(config) {
-    this.group_id = config.group_id;
-    this.auth_token = config.auth_token;
-    this.base_url = config.base_url;
+    this._baseUrl = config.baseUrl;
+    this._headers = config.headers;
   }
 
-  // вынес одинаковый обработчик ответа сервера в отдельную функцию
   useServerResponse(res) {
     return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
   }
 
-  // получение карточек с сервера
   getCardsList() {
-    return fetch(`${this.base_url}${this.group_id}/cards`,{
-      headers: {
-        authorization: this.auth_token
+    return fetch(
+      `${this._baseUrl}/cards`,
+      {
+        headers: this._headers
       }
-    })
+    )
     .then(res => this.useServerResponse(res));
   }
 
-  // добавление новой карточки
   addCard({ name, link }) {
-    return fetch(`${this.base_url}${this.group_id}/cards`,{
-      method: 'POST',
-      headers: {
-        authorization: this.auth_token,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name,
-        link
-      })
-    })
+    return fetch(
+      `${this._baseUrl}/cards`,
+      {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          name,
+          link
+        })
+      }
+    )
     .then(res => this.useServerResponse(res));
   }
 
-  // удаление карточки
   deleteCard(сardId) {
-    return fetch(`${this.base_url}${this.group_id}/cards/${сardId}`,{
-      method: 'DELETE',
-      headers: {
-        authorization: this.auth_token,
-        'Content-Type': 'application/json'
+    return fetch(
+      `${this._baseUrl}/cards/${сardId}`,
+      {
+        method: 'DELETE',
+        headers: this._headers
       }
-    })
+    )
     .then(res => this.useServerResponse(res));
   }
 
-  // добавление удаление лайка карточки
   setCardLike(сardId, like) {
-    return fetch(`${this.base_url}${this.group_id}/cards/likes/${сardId}`,{
-      method: like ? 'PUT' : 'DELETE',
-      headers: {
-        authorization: this.auth_token,
-        'Content-Type': 'application/json'
+    return fetch(
+      `${this._baseUrl}/cards/likes/${сardId}`,
+      {
+        method: like ? 'PUT' : 'DELETE',
+        headers: this._headers
       }
-    })
+    )
     .then(res => this.useServerResponse(res));
   }
 
-  // получение информации о пользователе
+
   getUserInfo() {
-    return fetch(`${this.base_url}${this.group_id}/users/me`,{
-      headers: {
-        authorization: this.auth_token
+    return fetch(
+      `${this._baseUrl}/users/me`,
+      {
+        headers: this._headers
       }
-    })
+    )
     .then(res => this.useServerResponse(res));
   }
 
-  // редактирование данных пользователя
   setUserInfo({name, about}) {
-    return fetch(`${this.base_url}${this.group_id}/users/me`,{
-      method: 'PATCH',
-      headers: {
-        authorization: this.auth_token,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name,
-        about
-      })
-    })
+    return fetch(
+      `${this._baseUrl}/users/me`,
+      {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({
+          name,
+          about
+        })
+      }
+    )
     .then(res => this.useServerResponse(res));
   }
 
-  // обновление аватара пользователя
   setUserPic(avatar) {
-    return fetch(`${this.base_url}${this.group_id}/users/me/avatar`,{
-      method: 'PATCH',
-      headers: {
-        authorization: this.auth_token,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        avatar
-      })
-    })
+    return fetch(
+      `${this._baseUrl}/users/me/avatar`,
+      {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({
+          avatar
+        })
+      }
+    )
     .then(res => this.useServerResponse(res));
   }
 }
 
-export default new Api(api_options);
+export default new Api(apiOptions);
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,1 +1,7 @@
-export const api_options = {group_id: 'cohort-44', auth_token: 'd496d52a-cde7-4ffc-b00b-f7d18fd5df30', base_url: 'https://mesto.nomoreparties.co/v1/'}
+export const apiOptions = {
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-44',
+  headers: {
+    authorization: 'd496d52a-cde7-4ffc-b00b-f7d18fd5df30',
+    'Content-Type': 'application/json'
+  }
+ }
